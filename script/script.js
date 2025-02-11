1b44f3a8e1d3c79a79a4f16d19cdc32dcbde741c
sammtan18 <143872025+sammtan18@users.noreply.github.com>
2025-02-11 11:41:53 -0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000
+    });
+});
+
+$('.service-card').waypoint(function() {
+    $('.service-card').addClass('pulse');
+}, {
+    offset: '100%'
+});
+
+$(function() {
+    new WOW().init();
+})
